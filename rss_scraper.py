7f4af8a22e625106e180468bb5fab600c5539305
aayush4vedi <4vedi.aayush@gmail.com>
2020-11-12 10:40:20 +0530

    #1: Added rss scraper for reddit & scheduled local cron job

--- rss_scraper.py
+++ rss_scraper.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from urllib.parse import urlparse
 
+import datetime
 import pymysql
 import pytz
 import scrapy
@@ -52,7 +53,7 @@ class RssScraperSpiderGeneric(scrapy.Spider):
                 table=table, columns=','.join(item.keys()), values=placeholder)
             self.cursor.execute(statement, list(item.values()))
             self.conn.commit()
-            print('Item: {} inserted to {}'.format(item, table))
+            # print('Item: {} inserted to {}'.format(item, table))
         except Exception as e:
             print('Error {} while inserting {}'.format(e, item))
             return False
@@ -110,27 +111,259 @@ class RssScraperSpiderReddit(scrapy.Spider):
     conn = pymysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
     cursor = conn.cursor()
     
-    start_urls = [
-        
+    subreddit_list = [
+        'compsci',
+        'computerscience',
+        'distributed',
+        'datastructures',
+        'algorithms',
+        'cpp_questions',
+        'GAMETHEORY',
+        'Discretemathematics',
+        'crypto',
+        'cryptography',
+        'logic',
+        'computerarchitecture',
+        'compilers',
+        'Network',
+        'ReverseEngineering',
+        'osdev',
+        'Android',
+        'MacOS',
+        'osx',
+        'windows',
+        'linux',
+        'kernel',
+        'linuxdev',
+        'linuxquestions',
+        'Ubuntu',
+        'hacking',
+        'HowToHack',
+        'Hacking_Tutorials',
+        'hackers',
+        'robotics',
+        'arduino',
+        'virtualreality',
+        'augmentedreality',
+        'IOT',
+        'computervision',
+        'opencv',
+        'imageprocessing',
+        'dip',
+        'datamining',
+        'textdatamining',
+        'MachineLearning',
+        'learnmachinelearning',
+        'ResearchML',
+        'neuralnetworks',
+        'neuralnetworks',
+        'NeuralNetwork',
+        'deeplearning',
+        'DeepLearningPapers',
+        'deeplearners',
+        'datascience',
+        'learndatascience',
+        'Database',
+        'datasets',
+        'statistics',
+        'AskStatistics',
+        'Rlanguage',
+        'rstats',
+        'matlab',
+        'scala',
+        'scikit_learn',
+        'JupyterNotebooks',
+        'kaggle',
+        'datacleaning',
+        'NLP',
+        'LanguageTechnology',
+        'bigdata',
+        'apachespark',
+        'hadoop',
+        'visualization',
+        'dataisbeautiful',
+        'tableau',
+        'excel',
+        'ExcelTips',
+        'artificial',
+        'ArtificialInteligence',
+        'softwaredevelopment',
+        'programming',
+        'coding',
+        'SoftwareEngineering',
+        'ProgrammingLanguages',
+        'learnprogramming',
+        'functionalprogramming',
+        'asm',
+        'C_Programming',
+        'c_language',
+        'cpp',
+        'Cplusplus',
+        'Python',
+        'scala',
+        'erlang',
+        'haskell',
+        'java',
+        'javascript',
+        'lisp',
+        'perl',
+        'PHP',
+        'ruby',
+        'rust',
+        'dotnet',
+        'Kotlin',
+        'html',
+        'html',
+        'css',
+        'rails',
+        'django',
+        'reactjs',
+        'git',
+        'github',
+        'gitlab',
+        'virtualization',
+        'browsers',
+        'aws',
+        'AWS_cloud',
+        'AZURE',
+        'azuredevops',
+        'kubernetes',
+        'k8s',
+        'docker',
+        'GCP',
+        'vim',
+        'neovim',
+        'vim_magic',
+        'emacs',
+        'appdev',
+        'AppDevelopment',
+        'iosdev',
+        'iOSProgramming',
+        'androiddev',
+        'devops',
+        'netsec',
+        'compsec',
+        'websec',
+        'computergraphics',
+        'web_design',
+        'UI_Design',
+        'UI_programming',
+        'UXDesign',
+        'UXResearch',
+        'UX_Design',
+        'webdev',
+        'softwaretesting',
+        'systems',
+        'tinycode',
+        'api',
+        'gamedev',
+        'programmingchallenges',
+        'opensource',
+        'AskComputerScience',
+        'forhire',
+        'cscareerquestions',
+        'interviewpreparations',
+        'csinterviewproblems',
+        'interviews',
+        'technology',
+        'TrueReddit',
+        'wikipedia',
+        'geek',
+        'skeptic',
+        'blog',
+        'COPYRIGHT',
+        'noip',
+        'cognitivescience',
+        'torrents',
+        'books',
+        'scifi',
+        'bookclub',
+        'writing',
+        'atheism',
+        'philosophy',
+        'history',
+        'AskHistorians',
+        'business',
+        'Flipping',
+        'freelance',
+        'Upwork',
+        'SaaS',
+        'SideProject',
+        'marketing',
+        'SEO',
+        'bigseo',
+        'SEO_Digital_Marketing',
+        'science',
+        'askscience',
+        'AskPhysics',
+        'chemistry',
+        'biology',
+        'medicine',
+        'neuroscience',
+        'geology',
+        'environment',
+        'Health',
+        'Physics',
+        'space',
+        'aerospace',
+        'quantum',
+        'QuantumPhysics',
+        'energy',
+        'FluidMechanics',
+        'engineering',
+        'electronics',
+        'ECE',
+        'ElectricalEngineering',
+        'AskEngineers',
+        'LearnEngineering',
+        'AskElectronics',
+        'MechanicalEngineering',
+        'EngineeringStudents',
+        'rocketry',
+        'aviation',
+        'nasa',
+        'spacex',
+        'aerodynamics',
+        'StructuralEngineering',
+        '3Dprinting',
+        'math',
+        'mathematics',
+        'calculus',
+        'DifferentialEquations',
+        'Algebra',
+        'GraphTheory',
+        'Economics',
+        'economy',
+        'finance',
+        'personalfinance',
+        'Accounting',
+        'invest',
+        'invest',
+        'BlockchainStartups',
+        'CryptoCurrency',
+        'Bitcoin',
+        'BitcoinBeginners',
+        'bitcointrading',
+        'BitcoinDiscussion'
     ]
+
+    start_urls = ['http://www.reddit.com/r/' + subreddit + '/.rss' for subreddit in subreddit_list]
 
     def parse(self, response):
         parsed_uri = urlparse(response.url)
-        print("\n\t\t ==> Running for URL#2: {}".format(parsed_uri.netloc))
+        print("\n\t\t ==> Running for URL#3: {}".format(parsed_uri.netloc + parsed_uri.path))
         response.selector.remove_namespaces()
         for data in response.css('entry'):
             title = data.css('title::text').get('').strip()
-            description = data.css('summary::text').get('').strip()
-            link = data.css('uri::text').get().strip()
+            description = data.css('content::text').get('').strip()
+            link = data.css('link').xpath("@href").extract()
 
             tags = data.css('category').xpath("@term").extract()
             item = {
-                'link': response.urljoin(link),
+                'link': link,
                 'title': title,
-                # https://www.aljazeera.com/xml/rss/all.xml becomes aljazeera.com
                 'source': '.'.join(parsed_uri.netloc.split('.')[-2:]),
-                # convert datetime to UTC datetime
-                'published_date': parse(data.css('published::text').get()).astimezone(pytz.utc).strftime('%Y-%m-%d %H:%M:%S'),
+                'published_date': parse(data.css('updated::text').get()).astimezone(pytz.utc).strftime('%Y-%m-%d %H:%M:%S'),
                 'tags': ','.join(tags)
             }
             self.insert_into_db('data', item)
@@ -156,7 +389,9 @@ if __name__ == "__main__":
 
     process.crawl(RssScraperSpiderGeneric)
     process.crawl(RssScraperSpiderStackoverflow)
+    process.crawl(RssScraperSpiderReddit)
     process.start() # the script will block here until the crawling is finished
+    print("\n\n ********************************************************** Ran Spiders at: {}\n\n".format(datetime.datetime.now()))
 
 
 ## Create table in db
@@ -166,24 +401,18 @@ if __name__ == "__main__":
 * use cynical_reader;
 * create table:
 
-    CREATE TABLE `data` (
+drop table data;
+CREATE TABLE `data` (
   `link` varchar(255) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   `source` varchar(255) DEFAULT NULL,
   `published_date` varchar(255) DEFAULT NULL,
   `tags` varchar(9999) DEFAULT NULL
 );
+CREATE UNIQUE INDEX uniq_idx
+ON cynical_reader.data(source,title,published_date);
 
-feed:nth-child(1) > entry:nth-child(8)
-/feed/entry[1]
 
-response.selector.register_namespace('namesp', 
-                                     'http://www.w3.org/2005/Atom')
-
-response.css('namesp:feed').get()                                     
-response.xpath('/namesp:feed/entry').get()      
-
-response.selector.remove_namespaces()
-response.css('feed').get()                                     
+25 * * * * /usr/local/bin/python3 /Users/aayush.chaturvedi/Sandbox/mc_cn/rss_scraper.py >> /Users/aayush.chaturvedi/Sandbox/mc_cn/cron_logs.txt 2>&1                            
 
 '''
